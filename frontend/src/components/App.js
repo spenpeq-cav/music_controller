c37527b81d40b4a187a63a8b9cd7bf5692d7d362
Spencer Pequegnat <spencer.peq@gmail.com>
2021-01-12 18:56:15 -0800

    Added components and react router

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
+import JoinRoomPage from "./JoinRoomPage";
+import CreateRoomPage from "./CreateRoomPage";
 
 export default class App extends Component{
     constructor(props){
         super(props);
+    
     }
     render() {
-        return (<h1>Testing React Code</h1>)
+        return (
+        <div>
+            <HomePage />
+        </div>)
     }
 }
 
---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import JoinRoomPage from "./JoinRoomPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
+
+export default class HomePage extends Component{
+    constructor(props){
+        super(props);
+    
+    }
+    render() {
+        return <Router>
+            <Switch>
+                <Route exact path='/'>
+                    <p>This is the Home Page</p>
+                </Route>
+                <Route path='/join' component={JoinRoomPage} />
+                <Route path='/create' component={CreateRoomPage} />
+            </Switch>
+        </Router>;
+    }
+}
+
