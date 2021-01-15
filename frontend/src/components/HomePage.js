966c23982c58595c5d0219494de88978d2889234
Spencer Pequegnat <spencer.peq@gmail.com>
2021-01-15 14:02:00 -0800

    Room styling, leave room functionality

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -11,6 +11,7 @@ export default class HomePage extends Component{
         this.state = {
             roomCode: null,
         };
+        this.clearRoomCode = this.clearRoomCode.bind(this);
     }
 
     async componentDidMount() {
@@ -41,6 +42,12 @@ export default class HomePage extends Component{
         );
     }
 
+    clearRoomCode() {
+        this.setState({
+            roomCode: null,
+        })
+    }
+
     render() {
         return <Router>
             <Switch>
@@ -49,7 +56,10 @@ export default class HomePage extends Component{
                 }} />
                 <Route path='/join' component={JoinRoomPage} />
                 <Route path='/create' component={CreateRoomPage} />
-                <Route path='/room/:roomCode' component={Room} />
+                <Route path='/room/:roomCode' 
+                    render={(props) => {
+                        return <Room {...props} leaveRoomCallback={this.clearRoomCode}/>
+                    }} />
             </Switch>
         </Router>;
     }
