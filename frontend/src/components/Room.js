fde7800f4f0cb0117a582eacd8dc1cc262ba0b50
Spencer Pequegnat <spencer.peq@gmail.com>
2021-01-16 15:47:07 -0800

    default props, fix/finish update button functionality, success/error message on update.

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -12,11 +12,12 @@ export default class Room extends Component {
         showSettings: false,
         }
         this.roomCode = this.props.match.params.roomCode;
-        this.getRoomDetails();
         this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
         this.updateShowSettings = this.updateShowSettings.bind(this);
         this.renderSettingsButton = this.renderSettingsButton.bind(this);
         this.renderSettings = this.renderSettings.bind(this);
+        this.getRoomDetails = this.getRoomDetails.bind(this);
+        this.getRoomDetails();
     }
 
     getRoomDetails() {
@@ -68,7 +69,7 @@ export default class Room extends Component {
         return (
         <Grid container spacing={1}>
             <Grid item xs={12} align="center">
-                <CreateRoomPage update={true} votesToSkip={this.state.votesToSkip} guestCanPause={this.state.guestCanPause} roomCode={this.roomCode} udpateCallback={() => {}} />
+                <CreateRoomPage update={true} votesToSkip={this.state.votesToSkip} guestCanPause={this.state.guestCanPause} roomCode={this.roomCode} updateCallback={this.getRoomDetails} />
             </Grid>
             <Grid item xs={12} align="center">
                 <Button variant="contained" color="secondary" onClick={() => this.updateShowSettings(false)}>
