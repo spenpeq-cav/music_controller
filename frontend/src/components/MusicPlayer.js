019fff8a549bc8cb5b6be9dc05050fc02fa87f7e
Spencer Pequegnat <spencer.peq@gmail.com>
2021-01-19 11:14:11 -0800

    Pausing and playing music with Spotify api

--- frontend/src/components/MusicPlayer.js
+++ frontend/src/components/MusicPlayer.js
@@ -9,6 +9,22 @@ export default class MusicPlayer extends Component {
         super(props);
     }
 
+    pauseSong() {
+        const requestOptions = {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+        };
+        fetch('/spotify/pause', requestOptions)
+    }
+
+    playSong() {
+        const requestOptions = {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+        };
+        fetch('/spotify/play', requestOptions)
+    }
+
     render() {
         const songProgress = (this.props.time / this.props.duration) * 100
 
@@ -22,7 +38,7 @@ export default class MusicPlayer extends Component {
                         <Typography component="h5" variant="h5"> {this.props.title} </Typography>
                         <Typography color="textSecondary" variant="subtitle1"> {this.props.artist} </Typography>
                         <div>
-                            <IconButton>
+                            <IconButton onClick={() => {this.props.is_playing ? this.pauseSong() : this.playSong()}}>
                                 {this.props.is_playing ? <PauseIcon/>: <PlayArrowIcon/>}
                             </IconButton>
                             <IconButton>
